e961872351e5d6ec9d038f90edbdd6c45c377001
groiq <35578027+groiq@users.noreply.github.com>
2018-12-09 11:19:42 +0100

    demo program

---
+++ src/playground/spring/demo/MyApp.java
@@ -0,0 +1,14 @@
+package playground.spring.demo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		// create the object
+		SkiingCoach theCoach = new SkiingCoach();
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}
---
+++ src/playground/spring/demo/SkiingCoach.java
@@ -0,0 +1,9 @@
+package playground.spring.demo;
+
+public class SkiingCoach {
+	
+	public String getDailyWorkout() {
+		return "spend 30 minutes bench-pressing";
+	}
+
+}
