5b3adf3262309ba0af566d5cb6181815ebc11b44
groiq <35578027+groiq@users.noreply.github.com>
2018-12-27 11:36:22 +0100

    print out cats

--- src/playground/spring/demo/CategoryCombinations.java
+++ src/playground/spring/demo/CategoryCombinations.java
@@ -2,9 +2,18 @@ package playground.spring.demo;
 
 public class CategoryCombinations {
 	
+	static String[][] cats = {{"human","dwarf","elf"},{"warrior","rogue","mage"}};
+//			[["human","dwarf","elf"],["warrior","rogue","mage"]];
 	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for (String[] cat: cats) {
+//			System.out.println(cat);
+			for (String item: cat) {
+				System.out.println(item);
+			}
+		}
 
 	}
 
