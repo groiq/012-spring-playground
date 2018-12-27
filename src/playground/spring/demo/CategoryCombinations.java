f45b06fcb7caf2ba4457a16ea1eec10c08129c2c
groiq <35578027+groiq@users.noreply.github.com>
2018-12-27 12:59:21 +0100

    first version of loop

--- src/playground/spring/demo/CategoryCombinations.java
+++ src/playground/spring/demo/CategoryCombinations.java
@@ -5,15 +5,28 @@ public class CategoryCombinations {
 	static String[][] cats = {{"human","dwarf","elf"},{"warrior","rogue","mage"}};
 //			[["human","dwarf","elf"],["warrior","rogue","mage"]];
 	
-
-	private static String getCombinations() {
+	private static String processOneEntry(String catsAbove, int level) {
 		String result = "";
-		for (String[] cat: cats) {
-//			System.out.println(cat);
-			for (String item: cat) {
-				result = result + item;
+		for (String entry : cats[level]) {
+//			System.out.println(entry);
+			result = result + " " + catsAbove + " " + entry;
+			if (level < cats.length - 1) {
+				result = result + processOneEntry(result, level+1);
+			} else {
+				result += ", ";
 			}
 		}
+		return result;
+	}
+	
+	private static String getCombinations() {
+		String result = processOneEntry("",0);
+//		for (String[] cat: cats) {
+//			System.out.println(cat);
+//			for (String item: cat) {
+//				result = result + item;
+//			}
+//		}
 		return result;
 	}
 	
