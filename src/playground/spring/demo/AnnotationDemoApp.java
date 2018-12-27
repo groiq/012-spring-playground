1a8421ee98ad74e936298678c2addfb017ee9ec9
groiq <35578027+groiq@users.noreply.github.com>
2018-12-27 08:03:39 +0100

    annotations exercise -set up main class

---
+++ src/playground/spring/demo/AnnotationDemoApp.java
@@ -0,0 +1,19 @@
+package playground.spring.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		
+		
+		context.close();
+		
+
+	}
+
+}
