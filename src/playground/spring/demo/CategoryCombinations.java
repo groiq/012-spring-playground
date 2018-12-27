f6acb14a214b9991b42344b59e29a3a93fada08a
groiq <35578027+groiq@users.noreply.github.com>
2018-12-27 12:44:41 +0100

    moved loop to method

--- src/playground/spring/demo/CategoryCombinations.java
+++ src/playground/spring/demo/CategoryCombinations.java
@@ -5,15 +5,20 @@ public class CategoryCombinations {
 	static String[][] cats = {{"human","dwarf","elf"},{"warrior","rogue","mage"}};
 //			[["human","dwarf","elf"],["warrior","rogue","mage"]];
 	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static String getCombinations() {
+		String result = "";
 		for (String[] cat: cats) {
 //			System.out.println(cat);
 			for (String item: cat) {
 				System.out.println(item);
 			}
 		}
+		return result;
+	}
+	
+
+	public static void main(String[] args) {
+		System.out.println(getCombinations());
 
 	}
 
