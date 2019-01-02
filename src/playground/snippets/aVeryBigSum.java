e6e7279f1ac84ad6c0caefaeb6a6db3548b2fd09
groiq <35578027+groiq@users.noreply.github.com>
2019-01-02 17:09:11 +0100

    range limit exercise: setup

---
+++ src/playground/snippets/aVeryBigSum.java
@@ -0,0 +1,29 @@
+package playground.snippets;
+
+/*
+ * after https://www.hackerrank.com/challenges/a-very-big-sum/problem
+ * 
+ * handling sums exceeding max range of a numerical data type.
+ * Thought: Instead of using a larger data type, track how much the sum *differs* from maximum.
+ * 
+ * Problem: Even if I use tricks while calculating a sum, I'd have to cast type to return the result.
+ * workaround: I won't calc a sum, instead I'll compare two sums.
+ * Input two arrays of numbers, return a boolean - true if first sum is larger than second.
+ * Ignore "both sums equal" cases for now.
+ * 
+ * Data type is byte, which is the smallest integer-ish data type in Java (-127-128).
+ */
+
+public class aVeryBigSum {
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+
+}
