7e39e8c44bdec0f300b13f910e319033fe0752a3
groiq <35578027+groiq@users.noreply.github.com>
2019-01-07 15:08:10 +0100

    set up factory

--- src/playground/hibernate/EmployeeDemo.java
+++ src/playground/hibernate/EmployeeDemo.java
@@ -1,20 +1,45 @@
 package playground.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class EmployeeDemo {
+	
+	static Session session;
+	
 
 	public static void main(String[] args) {
 		
 		// initialize everything
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfd.xml")
+				.addAnnotatedClass(Employee.class)
+				.buildSessionFactory();
 		
-		
-		// save objects
-		
-		// retrieve an object by primary key
-		
-		// query objects by company column
-		
-		// delete object by primary key
+		try {
+
+			// save objects
+			createEmployee();
+
+			// retrieve an object by primary key
+
+			// query objects by company column
+
+			// delete object by primary key
+
+
+		} finally {
+			factory.close();
+		}
+
 		
 	}
+
+	private static void createEmployee() {
+		System.out.println("Creating a new employee...");
+	}
+
+	
 
 }
