9362c37f8634c6c4aad5125be383465ca42810fe
groiq <35578027+groiq@users.noreply.github.com>
2019-01-07 14:54:55 +0100

    set up main app

---
+++ src/playground/hibernate/EmployeeDemo.java
@@ -0,0 +1,20 @@
+package playground.hibernate;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		
+		// initialize everything
+		
+		
+		// save objects
+		
+		// retrieve an object by primary key
+		
+		// query objects by company column
+		
+		// delete object by primary key
+		
+	}
+
+}
