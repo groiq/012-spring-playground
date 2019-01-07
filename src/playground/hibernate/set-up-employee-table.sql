a777765ba2c3175ea7528684d61b0b4f018478d6
groiq <35578027+groiq@users.noreply.github.com>
2019-01-07 13:11:19 +0100

    set up one-column table

--- src/playground/hibernate/set-up-employee-table.sql
+++ src/playground/hibernate/set-up-employee-table.sql
@@ -1,3 +1,9 @@
 USE `hb_student_tracker`;
 
 DROP TABLE IF EXISTS `employee`;
+
+
+CREATE TABLE `employee` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    PRIMARY  KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
