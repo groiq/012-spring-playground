ac37d1348eab4fda9e9c0b8750e1987da9c1ab17
groiq <35578027+groiq@users.noreply.github.com>
2019-04-09 20:10:44 +0200

    created a method that returns the start page

--- 012-02-simple-database-interaction/src/main/java/com/example/demo/MainController.java
+++ 012-02-simple-database-interaction/src/main/java/com/example/demo/MainController.java
@@ -1,8 +1,21 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 class MainController {
+	
+	@GetMapping("/index")
+	ModelAndView mapUrlToPage() {
+		
+		ModelAndView modelAndView = new ModelAndView("indexBackendSide");
+		modelAndView.setViewName("indexBackendSide"); // alternative to parametrized constructor
+		
+		return modelAndView;
+		
+	}
+	
 
 }
