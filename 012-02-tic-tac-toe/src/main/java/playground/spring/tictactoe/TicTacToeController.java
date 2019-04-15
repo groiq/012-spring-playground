2bab74f2e891f879a70171a1e0ea2d812761bd3e
groiq <35578027+groiq@users.noreply.github.com>
2019-04-15 17:13:47 +0200

    fill board correctly

--- 012-02-tic-tac-toe/src/main/java/playground/spring/tictactoe/TicTacToeController.java
+++ 012-02-tic-tac-toe/src/main/java/playground/spring/tictactoe/TicTacToeController.java
@@ -9,21 +9,41 @@ import org.springframework.web.servlet.ModelAndView;
 public class TicTacToeController {
 	
 	private int[][] boardData = new int[3][3];
-	
-	public TicTacToeController() {
-		for (int i = 0; i < boardData.length; i++) {
-			for (int j = 0; j < boardData[i].length; j++) {
-				boardData[i][j] = (i+j) % 3;
-			}
-		}
+
+	private void checkBoard(int row, int col) {
+		
+		// check for a win
+		
+		
+		// check for a draw
+		
 	}
 	
+	private void computerMoves() {
+		
+		for (int i = 0; i < boardData.length; i++) {
+			for (int j = 0; j < boardData[i].length; j++) {
+				if (boardData[i][j] == 0) {
+					boardData[i][j] = 2;
+					checkBoard(i,j);
+					return;
+				}
+			}
+		}
+		
+	}
+
 	@GetMapping("/tictactoe/{row}/{col}")
 	ModelAndView processMove(@PathVariable int row, @PathVariable int col) {
 		
 		System.out.println(row + " + " + col + " = " + (row+col));
 		
 		boardData[row][col] = 1;
+		
+		checkBoard(row,col);
+		
+		computerMoves();
+		
 		
 		// check if finished (won or draw)
 		
@@ -35,7 +55,19 @@ public class TicTacToeController {
 	}
 	
 	@GetMapping("/tictactoe")
+	ModelAndView resetGame() {
+		
+		for (int i = 0; i < boardData.length; i++) {
+			for (int j = 0; j < boardData[i].length; j++) {
+				boardData[i][j] = 0;
+			}
+		}
+		
+		return ticTacToeBoard();
+	}
+	
 	ModelAndView ticTacToeBoard() {
+		
 		
 		ModelAndView board = new ModelAndView("ticTacToe");
 		board.addObject("boardData",boardData);
