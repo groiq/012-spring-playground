2f3881e220e374f7f6321e12fba6c4ff57e5ae3c
groiq <35578027+groiq@users.noreply.github.com>
2019-04-15 13:03:01 +0200

    fill table with successive numbers

--- 012-02-tic-tac-toe/src/main/java/playground/spring/tictactoe/TicTacToeController.java
+++ 012-02-tic-tac-toe/src/main/java/playground/spring/tictactoe/TicTacToeController.java
@@ -11,7 +11,13 @@ public class TicTacToeController {
 	private int[][] boardData = new int[3][3];
 	
 	public TicTacToeController() {
-		
+		int counter = 0;
+		for (int i = 0; i < boardData.length; i++) {
+			for (int j = 0; j < boardData[i].length; j++) {
+				boardData[i][j] = counter;
+				counter++;
+			}
+		}
 	}
 	
 	@GetMapping("/tictactoe")
