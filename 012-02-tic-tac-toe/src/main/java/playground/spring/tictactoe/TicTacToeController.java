58a30e96340703143a749a6b4a1397fe006f8e29
groiq <35578027+groiq@users.noreply.github.com>
2019-04-16 07:48:19 +0200

    finalized game

--- 012-02-tic-tac-toe/src/main/java/playground/spring/tictactoe/TicTacToeController.java
+++ 012-02-tic-tac-toe/src/main/java/playground/spring/tictactoe/TicTacToeController.java
@@ -11,71 +11,89 @@ public class TicTacToeController {
 	private int[][] boardData = new int[3][3];
 	private String status = "open";
 
-	private void checkBoard(int row, int col) {
+	private boolean checkBoard(int row, int col) {
 		
+		boolean won = false;
 		int player = boardData[row][col];
 		
 		// check for a win
-		
+		boolean sameRow = true;
+		boolean sameCol = true;
+		boolean rising = true;
+		boolean falling = true;
+		for (int i = 0; i < boardData.length; i++) {
+			if (boardData[row][i] != player) {
+				sameRow = false;
+			}
+			if (boardData[i][col] != player) {
+				sameCol = false;
+			}
+			if (boardData[i][i] != player) {
+				rising = false;
+			}
+			if (boardData[i][2-i] != player)
+				falling = false;
+		}
+		if (sameRow || sameCol || rising || falling) {
+			won = true;
+		}
 		
 		// check for a draw (i.e. a full board)
+		boolean draw = true;
+		outer: for (int i = 0; i < boardData.length; i++) {
+			for (int j = 0; j < boardData[i].length; j++) {
+				if (boardData[i][j] == 0) {
+					draw = false;
+					break outer;
+				}
+			}
+		}
+		if (draw) { status = "draw"; }
+		
+		return won;
 		
 	}
 	
 	private void computerMoves() {
-		
-		for (int i = 0; i < boardData.length; i++) {
+		outer: for (int i = 0; i < boardData.length; i++) {
 			for (int j = 0; j < boardData[i].length; j++) {
 				if (boardData[i][j] == 0) {
 					boardData[i][j] = 2;
-					checkBoard(i,j);
-					return;
+					if (checkBoard(i,j)) {
+						status = "computer";
+					}
+					break outer;
 				}
 			}
 		}
-		
 	}
 
 	@GetMapping("/tictactoe/{row}/{col}")
 	ModelAndView processMove(@PathVariable int row, @PathVariable int col) {
-		
-//		System.out.println(row + " + " + col + " = " + (row+col));
-		
 		boardData[row][col] = 1;
-		
-		checkBoard(row,col);
-		
-		computerMoves();
-		
-		
-		// check if finished (won or draw)
-		
-		// computer makes his move
-		
-		// again check if finished
-		
+		if (checkBoard(row,col)) {
+			status = "user";
+		} else {
+			computerMoves();
+		}
 		return ticTacToeBoard();
 	}
 	
 	@GetMapping("/tictactoe")
 	ModelAndView resetGame() {
-		
+		status = "open";
 		for (int i = 0; i < boardData.length; i++) {
 			for (int j = 0; j < boardData[i].length; j++) {
 				boardData[i][j] = 0;
 			}
 		}
-		
 		return ticTacToeBoard();
 	}
 	
 	ModelAndView ticTacToeBoard() {
-		
-		
 		ModelAndView board = new ModelAndView("ticTacToe");
 		board.addObject("boardData",boardData);
 		board.addObject("status",status);
-		
 		return board;
 	}
 
