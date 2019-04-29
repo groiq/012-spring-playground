94756ff73905402a1829ad0f91fd401fac87e82b
groiq <35578027+groiq@users.noreply.github.com>
2019-04-29 16:09:01 +0200

    set up character table

--- 012-03-single-table-crud/src/main/java/playground/spring/crud/singletable/controller/PlayerCharacterController.java
+++ 012-03-single-table-crud/src/main/java/playground/spring/crud/singletable/controller/PlayerCharacterController.java
@@ -1,14 +1,24 @@
 package playground.spring.crud.singletable.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import playground.spring.crud.singletable.dao.PlayerCharacterRepository;
+
+@Controller
 public class PlayerCharacterController {
+	
+	@Autowired
+	PlayerCharacterRepository repo;
 	
 	@GetMapping("/")
 	public String listPlayerCharacters(Model theModel) {
 		
-		return "index.html";
+		theModel.addAttribute("characters",repo.findAll());
+		
+		return "singleTable.html";
 	}
 
 }
