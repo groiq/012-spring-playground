87ff74e964f6dc0b71c2f8136fa6ed3aee36afaa
groiq <35578027+groiq@users.noreply.github.com>
2019-04-29 16:53:07 +0200

    autofill doesn't work, but at least the rest does

--- 012-03-single-table-crud/src/main/java/playground/spring/crud/singletable/controller/PlayerCharacterController.java
+++ 012-03-single-table-crud/src/main/java/playground/spring/crud/singletable/controller/PlayerCharacterController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import playground.spring.crud.singletable.dao.PlayerCharacterRepository;
+import playground.spring.crud.singletable.entity.PlayerCharacter;
 
 @Controller
 public class PlayerCharacterController {
@@ -13,18 +15,24 @@ public class PlayerCharacterController {
 	@Autowired
 	PlayerCharacterRepository repo;
 	
-	@GetMapping("/autofill")
-	public String autofill(Model theModel) {
-		
-		return listPlayerCharacters(theModel);
-	}
+//	@PostMapping("/autofill")
+//	public String autofill(Model theModel) {
+//		
+//		repo.save(new PlayerCharacter("Gavin","human","warrior"));
+//		
+////		return listPlayerCharacters(theModel);
+//		return "playerCharacters";
+//	}
 	
 	@GetMapping("/")
+//	public ModelAndView listPlayerCharacters() {
+//		ModelAndView modelAndView = new ModelAndView("playerCharacters");
+//		modelAndView.addObject("theCharacters",repo.findAll());
+//		return modelAndView;
+//	}
 	public String listPlayerCharacters(Model theModel) {
-		
-		theModel.addAttribute("characters",repo.findAll());
-		
-		return "playerCharacters.html";
+		theModel.addAttribute("theCharacters", repo.findAll());
+		return "playerCharacters";
 	}
 
 }
