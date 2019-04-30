0bfab5a135bbe2c5fee1bdc5021b5fe773e07980
groiq <35578027+groiq@users.noreply.github.com>
2019-04-30 12:35:43 +0200

    display and delete work, some cleanup

--- 012-03-single-table-crud/src/main/java/playground/spring/crud/singletable/controller/PlayerCharacterController.java
+++ 012-03-single-table-crud/src/main/java/playground/spring/crud/singletable/controller/PlayerCharacterController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
 import playground.spring.crud.singletable.dao.PlayerCharacterRepository;
 import playground.spring.crud.singletable.entity.PlayerCharacter;
@@ -14,41 +16,47 @@ import playground.spring.crud.singletable.entity.PlayerCharacter;
 public class PlayerCharacterController {
 	
 	@Autowired
-	PlayerCharacterRepository repo;
+	private PlayerCharacterRepository repo;
 	
-//	@PostMapping("/autofill")
-//	public String autofill(Model theModel) {
-//		
-//		repo.save(new PlayerCharacter("Gavin","human","warrior"));
-//		
-////		return listPlayerCharacters(theModel);
-//		return "playerCharacters";
-//	}
-	private void autofill() {
-		repo.save(new PlayerCharacter("Gavin","human","warrior"));
-		repo.save(new PlayerCharacter("Gylfi","norn","guardian"));
-		repo.save(new PlayerCharacter("Chlegg","asura","necromancer"));
+	private String dbStatus;
+	
+	private String lastActionMsg = "(no action message)";
+	private void resetActionMsg() {
+		lastActionMsg = "(no action message)";
 	}
 	
-	@GetMapping("/")
-//	public ModelAndView listPlayerCharacters() {
-//		ModelAndView modelAndView = new ModelAndView("playerCharacters");
-//		modelAndView.addObject("theCharacters",repo.findAll());
-//		return modelAndView;
-//	}
-	public String listPlayerCharacters(Model theModel) {
-		List<PlayerCharacter> theCharacters = repo.findAll();
-		String dbStatus;
-		if (theCharacters.isEmpty()) {
-			dbStatus = "found empty and autofilled";
-			autofill();
-			theCharacters = repo.findAll();
+	private void checkDatabaseFilled() {
+		if (repo.count() == 0) {
+			repo.save(new PlayerCharacter("Gavin","human","warrior"));
+			repo.save(new PlayerCharacter("Gylfi","norn","guardian"));
+			repo.save(new PlayerCharacter("Chlegg","asura","necromancer"));
+			dbStatus = "empty and autofilled";
 		} else {
-			dbStatus = "found filled";
+			dbStatus = "filled";
 		}
-		theModel.addAttribute("theCharacters", theCharacters);
-		theModel.addAttribute("dbStatus", dbStatus);
-		return "playerCharacters";
 	}
+		
+	@GetMapping("/")
+	public ModelAndView listPlayerCharacters() {
+		ModelAndView modelAndView = new ModelAndView("playerCharacters");
+		checkDatabaseFilled();
+		modelAndView.addObject("theCharacters", repo.findAll());
+		modelAndView.addObject("dbStatus", dbStatus);
+		modelAndView.addObject("lastActionMsg", lastActionMsg);
+		resetActionMsg();
+		return modelAndView;
+	}
+	
+	@GetMapping("{id}/delete")
+	public String deleteCharacter(Model theModel, @PathVariable int id) {
+		repo.deleteById(id);
+		lastActionMsg = "Character deleted.";
+		return "redirect:/";
+	}
+	
+	// implement create
+	
+	// implement update
+
 
 }
