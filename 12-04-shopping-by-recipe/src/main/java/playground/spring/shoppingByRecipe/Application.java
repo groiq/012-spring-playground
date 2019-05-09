4618b3222f144851ab4531525af6609940e07d6b
groiq <35578027+groiq@users.noreply.github.com>
2019-05-09 15:46:45 +0200

    package name is shoppingByRecipe

--- 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/Application.java
+++ 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/Application.java
@@ -1,4 +1,4 @@
-package playground.spring;
+package playground.spring.shoppingByRecipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
