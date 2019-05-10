02226803fe40f9314b308161ba2c1ddf0e2711a3
groiq <35578027+groiq@users.noreply.github.com>
2019-05-10 10:55:12 +0200

    added constructor for shelf

--- 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/entities/Shelf.java
+++ 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/entities/Shelf.java
@@ -6,16 +6,20 @@ import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class Shelf {
 	
 	@Id
 	@GeneratedValue
 	private int id;
 	
+	@NonNull
 	private String name;
 
 }
