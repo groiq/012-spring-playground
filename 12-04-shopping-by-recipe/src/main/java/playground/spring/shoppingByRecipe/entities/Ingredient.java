e09b320e10e15561c42ef030ac3b2e2a53164f91
groiq <35578027+groiq@users.noreply.github.com>
2019-05-10 14:35:41 +0200

    added parametrized constructor for Ingredient

--- 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/entities/Ingredient.java
+++ 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/entities/Ingredient.java
@@ -7,18 +7,23 @@ import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class Ingredient {
 
 	@Id
 	@GeneratedValue
 	private int id;
 	
+	@NonNull
 	private String name;
 	
+	@NonNull
 	@ManyToOne
 	private Shelf shelf;
 	
