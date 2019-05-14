2ae9e742f63080538db38b01a1c1b3a9360450b6
groiq <35578027+groiq@users.noreply.github.com>
2019-05-14 11:05:36 +0200

    refined sample data

--- 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/sampleData/SampleDataFiller.java
+++ 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/sampleData/SampleDataFiller.java
@@ -13,31 +13,33 @@ import playground.spring.shoppingByRecipe.entities.Shelf;
 public class SampleDataFiller {
 	
 	private static String[] shelfNames = {"vegetables","side dishes","dairy"};
-	private static String[] ingredientNames = {"papric","rice","sour cream"};
-	private static String[] recipeNames = {"duvec","potato sauce","risotto"};
-	private static int[][] recipeToIngredients = {new int[] {0,1}, new int[] {0,2}, new int[] {1,2}};
+	private static String[] ingredientNames = {"papric","tomato","rice","sour cream","cream"};
+	private static int[] ingredientToShelf = {0,0,1,2,2};
+	private static String[] recipeNames = {"duvec","papric sauce","risotto"};
+	private static int[][] recipeToIngredients = {new int[] {0,1,2}, new int[] {0,1,3}, new int[] {2,4}};
 
 	public static void fill(ShelfRepository shelves, IngredientRepository ingredients, RecipeRepository recipes) {
 
 		List<Shelf> shelvesList = new ArrayList<Shelf>();
 		List<Ingredient> ingredientsList = new ArrayList<Ingredient>();
 		List<Recipe> recipesList = new ArrayList<Recipe>();
-
-		/*
-		 * this assumes that each shelf name from the list corresponds with an ingredient name at the same position in the other list.
-		 */
+		
 		for (int i = 0; i < shelfNames.length; i++) {
 			shelvesList.add(new Shelf(shelfNames[i], i));
-			ingredientsList.add(new Ingredient(ingredientNames[i], shelvesList.get(i)));
 		}
-
+		
+		for (int i = 0; i < ingredientNames.length; i++) {
+			ingredientsList.add(new Ingredient(ingredientNames[i], shelvesList.get(ingredientToShelf[i])));
+		}
+		
+		
 		for (int i = 0; i < recipeNames.length; i++) {
 			recipesList.add(new Recipe(recipeNames[i]));
 		}
 
 		for (int i = 0; i < recipeToIngredients.length; i++) {
 			for (int j = 0; j < recipeToIngredients[i].length; j++) {
-				recipesList.get(i).getIngredients().add(ingredientsList.get(j));
+				recipesList.get(i).getIngredients().add(ingredientsList.get(recipeToIngredients[i][j]));
 			}
 		}
 
