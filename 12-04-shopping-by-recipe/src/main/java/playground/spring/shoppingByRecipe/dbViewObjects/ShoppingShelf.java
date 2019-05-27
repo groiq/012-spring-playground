77b95f7b11086189cdb7041803a25fae30f322f6
groiq <35578027+groiq@users.noreply.github.com>
2019-05-27 16:00:59 +0200

    set up shopping list entity + dao

---
+++ 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/dbViewObjects/ShoppingShelf.java
@@ -0,0 +1,45 @@
+package playground.spring.shoppingByRecipe.dbViewObjects;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import playground.spring.shoppingByRecipe.entities.Ingredient;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Immutable
+@Subselect("select distinct shelf.id, shelf.name, shelf.position, ingredient.name as ingredient"
+		+ "from ingredient join shelf"
+		+ "on ingredient.shelf_id = shelf.id")
+public class ShoppingShelf {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@NonNull
+	private String name;
+	
+	@NonNull
+	private int position;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "shelf_id")
+	private Set<Ingredient> ingredients;
+	
+}
