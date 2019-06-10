0cb2829145d2c0c28e9d3bff536a65d497e4fbfd
groiq <35578027+groiq@users.noreply.github.com>
2019-06-10 11:11:51 +0200

    added query for ingredients by recipe

--- 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/dbViewObjects/IngredientInShelfRepository.java
+++ 12-04-shopping-by-recipe/src/main/java/playground/spring/shoppingByRecipe/dbViewObjects/IngredientInShelfRepository.java
@@ -1,7 +1,19 @@
 package playground.spring.shoppingByRecipe.dbViewObjects;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IngredientInShelfRepository extends JpaRepository<IngredientInShelf, Integer> {
+	
+	@Query(value = "select ingredient.id, ingredient.name, shelf.name as shelf, shelf.position "
+			+ "from ingredient join shelf join recipe_ingredients "
+			+ "where recipe_ingredients.recipe_id = :recipeId "
+			+ "and recipe_ingredients.ingredients_id = ingredient.id "
+			+ "and ingredient.shelf_id = shelf.id "
+			+ "order by shelf.position", nativeQuery = true)
+	public List<IngredientInShelf> shoppingListFor(@Param("recipeId") int recipeId);
 
 }
